82cf6535df8ae1b310a582e4a863920087178b08
udayakiranss <udaykiranss@icloud.com>
2018-07-31 00:25:51 +0530

    Added dyno

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def welcome_message:
-  return "Welcome to Python API"
-  
+def welcome_message():
+    return "Welcome to Python API"
+
+
 if __name__ == '__main__':
     app.run(debug=1)
